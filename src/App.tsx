ecb1bc4a8316c99a05a2f3f493c5e1e6381b8e5a
minhtu <minhtu.developer@gmail.com>
2022-05-12 00:24:50 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-
-  
   return (
     <div className="App">
       <header className="App-header">
